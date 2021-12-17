d3f174b51bc9447876ce494668071b65d7d3d15c
Athira K R <athirakrajan123@gmail.com>
2021-12-17 11:45:36 +0530

    ADMINMASTER

---
+++ HOSPITALMANAGEMENTSYSTEM/HOSPITALMANAGEMENTSYSTEM/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HOSPITALMANAGEMENTSYSTEM.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+        
+        public ActionResult AdminHome()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AddSpecialization()
+        {
+            return View();
+        }
+    }
+}
