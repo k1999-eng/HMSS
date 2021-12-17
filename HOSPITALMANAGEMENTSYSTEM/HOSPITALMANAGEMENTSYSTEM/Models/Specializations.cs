8e3456fe6fb4d80f70bfa88fd7ebf4c62571b893
Athira K R <athirakrajan123@gmail.com>
2021-12-17 15:42:29 +0530

    gffgh

--- HOSPITALMANAGEMENTSYSTEM/HOSPITALMANAGEMENTSYSTEM/Models/Specializations.cs
+++ HOSPITALMANAGEMENTSYSTEM/HOSPITALMANAGEMENTSYSTEM/Models/Specializations.cs
@@ -8,6 +8,19 @@ namespace HOSPITALMANAGEMENTSYSTEM.Models
     public class Specializations
     {
         public int spclId { get; set; }
-        public string specializationName { get; set; }
+        public Special specializationName { get; set; }
+    }
+
+
+    public enum Special
+    {
+        GeneralPhysicians,
+        Pediatricians,
+        GeneralSurgeon,
+        Cardiologist,
+        Dentist,
+        Dermatologists,
+        Gynecologist,
+        ENTSpecialist,
     }
 }
