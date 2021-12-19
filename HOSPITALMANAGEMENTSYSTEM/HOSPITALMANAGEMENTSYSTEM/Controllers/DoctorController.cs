398b57bb9cd0508816781a59320e7355848b983c
Krishnapriya-git <55920502+k1999-eng@users.noreply.github.com>
2021-12-19 10:19:45 +0530

    updated

--- HOSPITALMANAGEMENTSYSTEM/HOSPITALMANAGEMENTSYSTEM/Controllers/DoctorController.cs
+++ HOSPITALMANAGEMENTSYSTEM/HOSPITALMANAGEMENTSYSTEM/Controllers/DoctorController.cs
@@ -9,7 +9,31 @@ namespace HOSPITALMANAGEMENTSYSTEM.Controllers
     public class DoctorController : Controller
     {
         // GET: Doctor
-        public ActionResult Index()
+       public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Appointments()
+        {
+            return View();
+        }
+        public ActionResult Patients()
         {
             return View();
         }
